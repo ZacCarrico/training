cfea28e9d680667ef40294fed7bc25ee8fcebc98
ZacCarrico <z.carrico@gmail.com>
2020-08-31 22:20:41 -0500

    add can_reach_finish

---
+++ python/arrays/can_reach_finish.py
@@ -0,0 +1,22 @@
+# sort of like a board game, given an array you see if the values can get you to the end of the array
+# for example, [3,3,1,0,2,0,1] would get you to the nd of the array b/c you could hop from index 0 -> 1 -> 4 -> 6 -> end
+# example of what wouldn't get you to the end is [3,3,1,0,0,2,0,1]
+# write a function that returns True if you can reach the end or False if you can't
+import unittest
+
+
+def can_reach_finish(arr):
+    i = 0
+    furthest_space = arr[0]
+    while i <= furthest_space  and furthest_space <= (len(arr) - 1):
+        furthest_space = max(furthest_space, arr[i] + i)
+        i += 1
+    return furthest_space > (len(arr) - 1)
+
+class TestCanReachFinish(unittest.TestCase):
+    def test_can_reach_finish(self):
+        assert(can_reach_finish([3,3,1,0,2,0,1]) == True)
+        assert(can_reach_finish([3,3,1,0,0,2,0,1]) == False)
+
+if __name__ == "__main__":
+    unittest.main()
